7246826a6af1887ffa4f5a3d7ae88197dd27d5a5
Julian Prester <julian@julianprester.com>
2022-08-01 17:36:26 +1000

    Install momentjs

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({
       './node_modules/alpinejs/dist/cdn.min.js': './js/alpine.min.js',
     });
+    eleventyConfig.addPassthroughCopy({
+      './node_modules/moment/min/moment.min.js': './js/moment.min.js'
+    });
   
     return {
       htmlTemplateEngine: "njk",
