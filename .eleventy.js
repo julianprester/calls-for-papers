4fe4cb5d6908441a37f5ec0b43d35dc9247e598e
Julian Prester <julian@julianprester.com>
2022-08-01 19:50:39 +1000

    Fix favicon

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/data.json');
+    eleventyConfig.addPassthroughCopy('src/favicon');
     eleventyConfig.addPassthroughCopy({
       './node_modules/alpinejs/dist/cdn.min.js': './js/alpine.min.js',
     });
