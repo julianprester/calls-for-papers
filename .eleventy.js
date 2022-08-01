77d1177f58a60adf1e5785ea31799abe0c08fb4c
Julian Prester <julian@julianprester.com>
2022-08-01 16:50:07 +1000

    Install eleventy

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('src/data.json');
+    eleventyConfig.addPassthroughCopy({
+      './node_modules/alpinejs/dist/cdn.min.js': './js/alpine.js',
+    });
+  
+    return {
+      htmlTemplateEngine: "njk",
+      dir: {
+        input: 'src',
+        output: 'docs',
+        layouts: 'layouts',
+        includes: 'includes',
+        data: 'data',
+      }
+    }
+  }
