ae9f95aaa3e717bcaeb3bbe3c9ab28012eb82060
Julian Prester <julianprester@users.noreply.github.com>
2022-08-01 18:14:49 +1000

    Refactor structure (#2)
    * Move files
    
    * Update paths
    
    * Install eleventy
    
    * Install tailwindcss
    
    * Install alpinejs
    
    * Install momentjs
    
    * Remove build from versioning
    
    * Transition to eleventy structure

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('src/data.json');
+    eleventyConfig.addPassthroughCopy({
+      './node_modules/alpinejs/dist/cdn.min.js': './js/alpine.min.js',
+    });
+    eleventyConfig.addPassthroughCopy({
+      './node_modules/moment/min/moment.min.js': './js/moment.min.js'
+    });
+  
+    return {
+      htmlTemplateEngine: "njk",
+      dir: {
+        input: 'src',
+        output: 'public',
+        layouts: 'layouts',
+        includes: 'includes',
+        data: 'data',
+      }
+    }
+  }
