c86e58bfd663e6ecd277dc507392f3a937181599
Julian Prester <julian@julianprester.com>
2022-08-16 19:58:28 +1000

    Refine JAIS scraper

--- scrapers/journals/jaisScraper.js
+++ scrapers/journals/jaisScraper.js
@@ -5,7 +5,7 @@ const scraperObject = {
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url, { waitUntil: 'networkidle2' });
 
-        return await page.$$eval('div#main > div#breadcrumb + h2 + ul li', items => items.map(item => {
+        return await page.$$eval('div#main > div#breadcrumb + h2 + ul li:not(br ~ li)', items => items.map(item => {
             return {
                 title: item.querySelector('a').textContent.replace(/[\t\n\r]/gm, '').trim(),
                 url: item.querySelector('a').href,
