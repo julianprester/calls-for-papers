5a97b0904400dde2037c55c03a4682cbce4a8db4
Julian Prester <julian@julianprester.com>
2022-08-16 19:36:07 +1000

    Fix Elsevier scrapers

--- scrapers/journals/imScraper.js
+++ scrapers/journals/imScraper.js
@@ -5,10 +5,10 @@ const scraperObject = {
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url, { waitUntil: 'networkidle2' });
 
-        return await page.$$eval('main > div > ul > li > article > a', items => items.map(item => {
+        return await page.$$eval('main h3', items => items.map(item => {
             return {
                 title: item.textContent,
-                url: item.href,
+                url: 'https://www.journals.elsevier.com/information-and-management/call-for-papers#' + item.id,
                 dueDate: null,
                 journal: 'Information and Management',
                 abbreviation: 'im'
