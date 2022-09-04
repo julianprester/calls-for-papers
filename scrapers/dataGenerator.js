47782a2cbb91b260484a7c344d070e21a4c05331
Julian Prester <julian@julianprester.com>
2022-09-04 16:09:16 +1000

    Move to dataGenerator

---
+++ scrapers/dataGenerator.js
@@ -0,0 +1,25 @@
+const fs = require('fs/promises');
+
+async function writeData(issues) {
+    const now = new Date();
+    let existingIssues = await readData();
+    issues.forEach(issue => {
+        issue.pubDate = Date.parse(existingIssues.find(item => item.url === issue.url) ? new Date(existingIssues.find(item => item.url === issue.url).pubDate) : now);
+    });
+    fs.writeFile("./www/data.json", JSON.stringify(issues, null, 2), err => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
+
+async function readData() {
+    try {
+        const data = await fs.readFile("./www/data.json", { encoding: 'utf8' });
+        return await JSON.parse(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = (issues) => writeData(issues)
