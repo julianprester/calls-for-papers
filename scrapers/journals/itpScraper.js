0e27a4c760db895a8620707cd30e6e8ffd78f98c
Julian Prester <hi@julianprester.com>
2023-09-12 11:19:59 +1000

    Fix ITP scraper

--- scrapers/journals/itpScraper.js
+++ scrapers/journals/itpScraper.js
@@ -1,14 +1,14 @@
 const scraperObject = {
-    url: 'https://www.emeraldgrouppublishing.com/journal/itp#calls-for-papers',
+    url: 'https://www.emeraldgrouppublishing.com/journal/itp',
     async scraper(browser) {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url, { waitUntil: 'networkidle2' });
 
-        return await page.$$eval('div#calls-for-papers a', items => items.map(item => {
+        return await page.$$eval('div.card__cfp', items => items.map(item => {
             return {
-                title: item.textContent,
-                url: item.href,
+                title: item.querySelector('div.card__cfp--content h2').textContent,
+                url: item.querySelector('a').href,
                 dueDate: null,
                 journal: 'Information Technology & People',
                 abbreviation: 'itp'
