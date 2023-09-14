f306d240f900aadb5ba7fe81776bf5844c652f20
Julian Prester <hi@julianprester.com>
2023-09-14 17:13:03 +1000

    Update timeout

--- scrapers/journals/misqScraper.js
+++ scrapers/journals/misqScraper.js
@@ -3,7 +3,7 @@ const scraperObject = {
     async scraper(browser) {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
-        await page.goto(this.url, { timeout: 60000 });
+        await page.goto(this.url, { timeout: 300000 });
         await page.hover('nav ul li:first-child');
         
         return await page.$$eval('nav ul li:first-child section.ammenu-submenu-block p a[href*="/call_for_papers/"]', items => items.map(item => {
