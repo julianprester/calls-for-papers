df487875072ebdf575badf5501dda92067dfec19
Julian Prester <hi@julianprester.com>
2023-09-21 17:39:31 +1000

    Fix EJIS scraper

--- scrapers/journals/ejisScraper.js
+++ scrapers/journals/ejisScraper.js
@@ -1,11 +1,11 @@
 const scraperObject = {
-    url: 'https://orsociety.tandfonline.com/action/newsAndOffers?journalCode=tjis20',
+    url: 'https://www.tandfonline.com/journals/tjis20',
     async scraper(browser) {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url);
 
-        return await page.$$eval('div.call-paper-widget > h2 + a', items => items.map(item => {
+        return await page.$$eval('div.cfpContent a', items => items.map(item => {
             return {
                 title: item.textContent,
                 url: item.href,
