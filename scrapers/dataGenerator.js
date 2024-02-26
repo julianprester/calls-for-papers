2e4c5c2c364ff5fcada4699c32dd2dd8e49ac2c3
Julian Prester <hi@julianprester.com>
2024-02-26 20:19:06 +1100

    Implement robust update logic

--- scrapers/dataGenerator.js
+++ scrapers/dataGenerator.js
@@ -1,12 +1,30 @@
 const fs = require('fs/promises');
 
-async function writeData(issues) {
+async function writeData(scrapedIssues) {
     const now = new Date();
     let existingIssues = await readData();
-    issues.forEach(issue => {
-        issue.pubDate = Date.parse(existingIssues.find(item => item.slug === issue.slug) ? new Date(existingIssues.find(item => item.slug === issue.slug).pubDate) : now);
-    });
-    fs.writeFile("./www/data.json", JSON.stringify(issues, null, 2), err => {
+
+    let scraped_not_existing = scrapedIssues.filter(issue => !existingIssues.some(existingIssue => existingIssue.slug === issue.slug));
+    let existing_not_scraped = existingIssues.filter(issue => !scrapedIssues.some(scrapedIssue => scrapedIssue.slug === issue.slug));
+    let existing_and_scraped = scrapedIssues.filter(issue => existingIssues.some(existingIssue => existingIssue.slug === issue.slug));
+
+    for (let issue of existingIssues) {
+        if (existing_not_scraped.find(item => item.slug === issue.slug)) {
+            issue.active = false;
+        }
+        if (existing_and_scraped.find(item => item.slug === issue.slug)) {
+            issue.active = true;
+            issue.url = existing_and_scraped.find(item => item.slug === issue.slug).url;
+            issue.dueDate = existing_and_scraped.find(item => item.slug === issue.slug).dueDate;
+        }
+    }
+
+    for (let issue of scraped_not_existing) {
+        issue.pubDate = Date.parse(now);
+    }
+    existingIssues = existingIssues.concat(scraped_not_existing);
+
+    fs.writeFile("./www/data.json", JSON.stringify(existingIssues, null, 2), err => {
         if (err) {
             console.error(err);
         }
