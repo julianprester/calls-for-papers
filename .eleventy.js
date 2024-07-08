767147b3fc6145a7ddb73f78930eb4860ece6dca
Julian Prester <hi@julianprester.com>
2024-07-08 15:13:46 +1000

    Remove moment dependency

--- .eleventy.js
+++ .eleventy.js
@@ -12,14 +12,10 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addShortcode("currentYear", () => `${new Date().getFullYear()}`);
 
-  eleventyConfig.addPassthroughCopy('www/data.json');
   eleventyConfig.addPassthroughCopy('www/rss.xml');
   eleventyConfig.addPassthroughCopy('www/favicon');
   eleventyConfig.addPassthroughCopy({
     './node_modules/alpinejs/dist/cdn.min.js': './js/alpine.min.js',
-  });
-  eleventyConfig.addPassthroughCopy({
-    './node_modules/moment/min/moment.min.js': './js/moment.min.js'
   });
 
   return {
