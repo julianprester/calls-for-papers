d7a22fe33e740cd6a8ec2ba010b0e36ba52758c0
Julian Prester <hi@julianprester.com>
2024-10-16 16:31:00 +1100

    Update MISQ scraper

--- scrapers/journals/misqScraper.js
+++ scrapers/journals/misqScraper.js
@@ -1,20 +1,38 @@
 const scraperObject = {
-    url: 'https://misq.umn.edu/',
+    url: 'https://misq.umn.edu/special-issues',
     async scraper(browser) {
         let page = await browser.newPage();
-        await page.goto(this.url, {waitUntil: 'domcontentloaded'});
-        await page.locator('css=nav ul li:first-child').hover();
-        await page.locator('css=nav ul li:first-child section.ammenu-submenu-block p a[href*="/call_for_papers/"]')
-        await page.waitForTimeout(15000)
-        
-        return await page.$$eval('nav ul li:first-child section.ammenu-submenu-block p a[href*="/call_for_papers/"]', items => items.map(item => {
+        await page.goto(this.url, { waitUntil: 'domcontentloaded' });
+
+        return await page.$$eval('div[data-content-type="text"]:nth-child(3) p:has(span > strong), div[data-content-type="text"]:nth-child(3) p:has(span > strong) ~ p:has(a)', items => items.map(item => {
+            const titleElement = item.querySelector('span > strong');
+            const title = titleElement ? titleElement.textContent.trim() : '';
+
+            let url = '';
+            let dueDate = '';
+            let currentElement = item.nextElementSibling;
+
+            while (currentElement && !currentElement.querySelector('span > strong')) {
+                const linkElement = currentElement.querySelector('a[href^="https://misq.umn.edu/"]');
+                if (linkElement && !url) {
+                    url = linkElement.href;
+                }
+
+                const dueDateMatch = currentElement.textContent.match(/Submission Deadline: (.+?)Expected Publication/);
+                if (dueDateMatch && !dueDate) {
+                    dueDate = Date.parse(dueDateMatch[1].trim());
+                }
+
+                currentElement = currentElement.nextElementSibling;
+            }
+
             return {
-                title: item.title,
-                url: item.href,
-                dueDate: null,
+                title: title,
+                url: url,
+                dueDate: dueDate,
                 journal: 'MIS Quarterly',
                 abbreviation: 'misq'
-            }
+            };
         }));
     }
 }
