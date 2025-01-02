b1df679bb486abe2d8921329421d1dbcb5c616da
Julian Prester <hi@julianprester.com>
2025-01-02 20:28:04 +1100

    Move to puppeteer-real-browser

--- scrapers/browser.mjs
+++ scrapers/browser.mjs
@@ -1,27 +1,14 @@
-import { chromium } from 'playwright-extra';
-import StealthPlugin from 'puppeteer-extra-plugin-stealth'
-
-StealthPlugin().enabledEvasions.delete('iframe.contentWindow')
-chromium.use(StealthPlugin())
+import { connect } from 'puppeteer-real-browser';
 
 export async function startBrowser() {
-    let browser;
-    try {
-        console.log("Opening the browser...")
-        browser = await chromium.launch({
-            headless: true,
-            ignoreHTTPSErrors: true,
-            args: [
-                "--no-sandbox",
-                "--window-size=1920,1080"
-            ],
-            defaultViewport: {
-                width: 1920,
-                height: 1080
-            }
-        })
-    } catch (err) {
-        console.log("Could not create a browser instance => : ", err)
-    }
-    return browser
+    const { _, browser } = await connect({
+        headless: false,
+        args: [],
+        customConfig: {},
+        turnstile: true,
+        connectOption: {},
+        disableXvfb: false,
+        ignoreAllFlags: false
+    });
+    return browser;
 }
