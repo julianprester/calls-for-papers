ed0bc27cf108399a530215c7990a951258cded79
Julian Prester <hi@julianprester.com>
2025-01-19 11:43:32 +1100

    Follow pages on Elsevier

--- scrapers/journals/jsisScraper.mjs
+++ scrapers/journals/jsisScraper.mjs
@@ -8,15 +8,30 @@ export const scraperObject = {
             return [];
         }
 
-        return await page.$$eval('main div:has(> h3)', items => items.map(item => {
+        let calls = await page.$$eval('main h3 a', items => items.map(item => {
             return {
                 journal: 'The Journal of Strategic Information Systems',
                 abbreviation: 'jsis',
-                metaTitle: item.querySelector('h3').textContent,
-                url: 'https://www.sciencedirect.com/journal/the-journal-of-strategic-information-systems/about/call-for-papers#' + item.querySelector('h3').id,
-                rawContent: item.innerHTML,
+                metaTitle: item.textContent,
+                url: item.href,
             }
         }));
+
+        calls = await Promise.all(
+            calls.map(async (call) => ({
+                ...call,
+                rawContent: await get_raw_content(browser, call.url)
+            }))
+        );
+
+        return calls;
     }
 }
 
+async function get_raw_content(browser, url) {
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    const rawContent = await page.$eval('div.inner', element => element.innerHTML);
+    await page.close();
+    return rawContent;
+}
